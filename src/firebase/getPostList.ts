5b455f3065f021f7b82ee20bf61eac0f5948eded
Your Name <code.creative814@gmail.com>
2024-01-06 01:00:43 +0900

    firebase: getPost and getPostList file

--- src/firebase/getPostList.ts
+++ src/firebase/getPostList.ts
@@ -5,7 +5,6 @@ export default async function getPostDocument() {
   const postId: any[] = [];
   const coll = collection(db, "post");
   const result = getDocs(coll);
-
   (await result).forEach((post) => postId.push(post.id));
 
   return postId;
