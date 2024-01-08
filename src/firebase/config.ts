6c34bc3a56c95f1f941460f02d975a3d9b9bc8e7
Your Name <code.creative814@gmail.com>
2024-01-09 00:34:54 +0900

    code update

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
